64f9221513ba61ff8329818d480d7ec1ba3fc361
Jérôme <jerome.audiard@gmail.com>
2024-07-21 15:52:28 +0200

    update CSS. add animation

---
+++ assets/js/javascript.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const containers = document.querySelectorAll('.container');
+
+    const options = {
+        root: null, 
+        rootMargin: '0px',
+        threshold: 0.25 // 50% du container doit etre visible
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            } else {
+                // entry.target.classList.remove('active'); // 
+            }
+        });
+    }, options);
+
+    containers.forEach(container => {
+        observer.observe(container);
+    });
+});
+
+
+console.log("JS chargé");
--- functions.php
+++ functions.php
@@ -21,6 +21,10 @@ add_action( 'wp_enqueue_scripts', 'jeblocs_enqueue_styles' );
 function jeblocs_enqueue_styles(){
 	//css du theme
 	wp_enqueue_style('jeblocs-style', get_stylesheet_directory_uri() . '/assets/css/theme.min.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.min.css'));
+    //css animation du theme
+	wp_enqueue_style('jeblocs-style-animation', get_stylesheet_directory_uri() . '/assets/css/animation.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/animation.css'));
+    //Script JS menu
+	wp_enqueue_script( 'jeblocs-javascript', get_stylesheet_directory_uri() . '/assets/js/javascript.js', array(), filemtime(get_stylesheet_directory() . '/assets//js/javascript.js'), true );
     };
 
 
