72aa7e2f809975e4b9dbd3a5cc61f7ebadec9cb1
Jérôme <jerome.audiard@gmail.com>
2024-10-01 16:23:13 +0200

    Ajout modal contact + Easter egg doom

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,5 @@
+// Effet d'apparition des sections (ajoute la class active)
+
 document.addEventListener('DOMContentLoaded', () => {
     const containers = document.querySelectorAll('.container');
 
@@ -23,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // doom via cheatcode
-const targetSequence = 'iddad';
+const targetSequence = 'iddqd';
 let currentSequence = '';
 
 // Fonction pour vérifier la séquence de touches
@@ -48,83 +50,94 @@ function handleKeyPress(event) {
 
 document.addEventListener('keydown', handleKeyPress);
 
-//gestion de la modale de contact (+ Doom)
+// gestion de la modale de contact
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const openModalButtons = document.querySelectorAll('.openModal');
-//     const modal = document.querySelector('.modal1');
-//     const closeModalButton = modal.querySelector('.close');
-
-//     openModalButtons.forEach(function(button) {
-//         button.addEventListener('click', function() {
-//             modal.classList.remove('hide');
-//             modal.classList.add('show');
-//         });
-//     });
-
-//     closeModalButton.addEventListener('click', function() {
-//         closeModal();
-//     });
-
-//     modal.addEventListener('click', function(event) {
-//         if (event.target === modal) {
-//             closeModal();
-//         }
-//     });
-
-//     // Ajouter un écouteur d'événement pour la touche Échap
-//     document.addEventListener('keydown', function(event) {
-//         if (event.key === 'Escape') { // Vérifie si la touche appuyée est Échap
-//             closeModal();
-//         }
-//     });
-
-//     // Fonction pour fermer la modal
-//     function closeModal() {
-//         modal.classList.remove('show');
-//         modal.classList.add('hide');
-        
-        
-//         setTimeout(function() {
-//             modal.classList.remove('hide');
-//         }, 300); // durée de l'animation de fin
-//     }
-// });
-
-//gestion de la modale de doom
-
-// document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.querySelector('.modalContact');
     
-//     const button = document.querySelector('.openModalDoom');
-//     const modalFrame = document.querySelector('.modalFrame');
+    if (!modal) return; // Assure que la modal existe avant d'ajouter les événements
 
-//     // Code HTML à ajouter
-//     const newContent = `
-//         <div class="modal2">
-//             <div class="modal2Wrapper">
-//                 <span class="close"></span>
-//                 <iframe src="https://audiard-jerome.github.io/doom/" ></iframe>
-//             </div>
-//         </div>
-//         `;
+    const openModalButtons = document.querySelectorAll('.openModal');
+    const closeModalButton = modal.querySelector('.close');
 
-//     // Ajoute un écouteur d'événements pour le clic sur le bouton
-//     button.addEventListener('click', function() {
-//         // Ajoute le nouveau contenu à la fin de modalFrame
-//         modalFrame.insertAdjacentHTML('beforeend', newContent);
+    // Vérifie si des boutons d'ouverture de la modal existent
+    if (openModalButtons.length > 0) {
+        openModalButtons.forEach(function(button) {
+            button.addEventListener('click', openModal);
+        });
+    }
 
-//         // Sélectionne l'élément close après l'ajout du contenu
-//         const closeButton = modalFrame.querySelector('.modal2 .close');
+    // Ajoute les écouteurs d'événements pour fermer la modal
+    if (closeModalButton) {
+        closeModalButton.addEventListener('click', closeModal);
+    }
 
-//         // Ajoute un écouteur d'événements pour le clic sur le bouton de fermeture
-//         closeButton.addEventListener('click', function() {
-//             // Supprime la modale
-//             const modal = modalFrame.querySelector('.modal2');
-//             if (modal) {
-//                 modal.remove();
-//             }
-//         });
-//     });
-// });
+    modal.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            closeModal();
+        }
+    });
+
+    // Fonction pour ouvrir la modal
+    function openModal() {
+        modal.classList.remove('hide');
+        modal.classList.add('show');
+    }
+
+    // Fonction pour fermer la modal
+    function closeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        
+        setTimeout(function() {
+            modal.classList.remove('hide');
+        }, 300); // durée de l'animation de fin
+    }
+});
+
+
+// gestion de la modale de doom
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const button = document.querySelector('.openModalDoom');
+    const modalFrame = document.querySelector('.modalFrame');
+
+    if (!modalFrame) return; // Assure que la modal existe avant d'ajouter les événements
+
+    // Code HTML à ajouter
+    const newContent = `
+        <div class="modal2">
+            <div class="modal2Wrapper">
+                <span class="close"></span>
+                <iframe src="https://audiard-jerome.github.io/doom/" ></iframe>
+            </div>
+        </div>
+        `;
+
+    // Ajoute un écouteur d'événements pour le clic sur le bouton
+    button.addEventListener('click', function() {
+        // Ajoute le nouveau contenu à la fin de modalFrame
+        modalFrame.insertAdjacentHTML('beforeend', newContent);
+
+        // Sélectionne l'élément close après l'ajout du contenu
+        const closeButton = modalFrame.querySelector('.modal2 .close');
+
+        // Ajoute un écouteur d'événements pour le clic sur le bouton de fermeture
+        closeButton.addEventListener('click', function() {
+            // Supprime la modale
+            const modal = modalFrame.querySelector('.modal2');
+            if (modal) {
+                modal.remove();
+            }
+        });
+    });
+});
 
 
